c033ed0ce99a487ac26ae45e3256fa30208e792d
Suleman Aswat <sulemanaswat@Sulemans-MacBook-Air.local>
2025-03-24 11:29:51 -0700

    Adding all week's files

---
+++ week8/index.js
@@ -0,0 +1,8 @@
+console.log(Math.floor(10 + Math.random() * 90)); //0(inclusive) to 1(exclusive)
+// 0.7657664656
+// 0.5435436532
+// 0.3216541756
+
+// 0.99 * 100 = 99.99 + 10 = 109.99
+
+// Math.floor(min + Math.random() * (max-min+1))
